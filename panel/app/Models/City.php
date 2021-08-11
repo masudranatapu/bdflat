d44853abadc099b1f3dedb12eccb760d3e915ed8
Monowar Hossain Khan <mhkha2022@gmail.com>
2021-08-12 02:11:04 +0600

    City/Division, Area

--- panel/app/Models/City.php
+++ panel/app/Models/City.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Model;
 class City extends Model
 {
     protected $table = 'SS_CITY';
-    protected $primaryKey 	= 'PK_NO';
-    public $timestamps 		= false;
+    protected $primaryKey = 'PK_NO';
+    public $timestamps = false;
 
-    public function getCity(){
+    public function getCity()
+    {
         return City::pluck('CITY_NAME', 'PK_NO');
     }
 }
---
+++ panel/app/Repositories/Admin/Area/AreaInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Repositories\Admin\Area;
+
+
+interface AreaInterface
+{
+    public function getAreas($limit = 2000);
+
+    public function getArea(int $id);
+
+    public function postStore($request);
+
+    public function postUpdate($request, int $id);
+}
