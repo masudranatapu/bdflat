21ec83dc9ae393327b35755963fd373ea1154c1e
Monowar Hossain Khan <mhkha2022@gmail.com>
2021-08-21 20:57:10 +0600

    Property Category

--- web/app/Http/Controllers/HomeController.php
+++ web/app/Http/Controllers/HomeController.php
@@ -2,21 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PropertyType;
 use App\Models\Slider;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     protected $slider;
+    protected $propertyType;
 
-    public function __construct(Slider $slider)
+    public function __construct(Slider $slider, PropertyType $propertyType)
     {
         $this->slider = $slider;
+        $this->propertyType = $propertyType;
     }
 
     public function index()
     {
         $data['sliders'] = $this->slider->getSliders();
+        $data['categories'] = $this->propertyType->getPropertyTypes();
         return view('home.home', compact('data'));
     }
 }
