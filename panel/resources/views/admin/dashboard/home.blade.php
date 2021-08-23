a620ce8cf982a9ff85978cfebb2eec08f6175978
unknown <mahi_rbs>
2021-08-24 01:09:19 +0600

    ..

--- panel/resources/views/admin/dashboard/home.blade.php
+++ panel/resources/views/admin/dashboard/home.blade.php
@@ -19,14 +19,34 @@
                             <div class="media d-flex">
                                 <div class="media-body text-left">
                                     <h3 class="info">850</h3>
-                                    <h6>Products Sold</h6>
+                                    <h6>Total Properties</h6>
                                 </div>
                                 <div>
                                     <i class="icon-basket-loaded info font-large-2 float-right"></i>
                                 </div>
                             </div>
                             <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                                <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0"
+                                <div class="progress-bar bg-gradient-x-info" role="progressbar" style="width: 80%" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-3 col-lg-6 col-12">
+                <div class="card pull-up">
+                    <div class="card-content">
+                        <div class="card-body">
+                            <div class="media d-flex">
+                                <div class="media-body text-left">
+                                    <h3 class="danger">99.89 %</h3>
+                                    <h6>Total Published Properties</h6>
+                                </div>
+                                <div>
+                                    <i class="icon-heart danger font-large-2 float-right"></i>
+                                </div>
+                            </div>
+                            <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
+                                <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0"
                                      aria-valuemax="100"></div>
                             </div>
                         </div>
@@ -40,7 +60,7 @@
                             <div class="media d-flex">
                                 <div class="media-body text-left">
                                     <h3 class="warning">$748</h3>
-                                    <h6>Net Profit</h6>
+                                    <h6>Total Owner</h6>
                                 </div>
                                 <div>
                                     <i class="icon-pie-chart warning font-large-2 float-right"></i>
@@ -61,7 +81,7 @@
                             <div class="media d-flex">
                                 <div class="media-body text-left">
                                     <h3 class="success">146</h3>
-                                    <h6>New Customers</h6>
+                                    <h6>Total Seeker</h6>
                                 </div>
                                 <div>
                                     <i class="icon-user-follow success font-large-2 float-right"></i>
@@ -75,27 +95,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xl-3 col-lg-6 col-12">
-                <div class="card pull-up">
-                    <div class="card-content">
-                        <div class="card-body">
-                            <div class="media d-flex">
-                                <div class="media-body text-left">
-                                    <h3 class="danger">99.89 %</h3>
-                                    <h6>Customer Satisfaction</h6>
-                                </div>
-                                <div>
-                                    <i class="icon-heart danger font-large-2 float-right"></i>
-                                </div>
-                            </div>
-                            <div class="progress progress-sm mt-1 mb-0 box-shadow-2">
-                                <div class="progress-bar bg-gradient-x-danger" role="progressbar" style="width: 85%" aria-valuenow="85" aria-valuemin="0"
-                                     aria-valuemax="100"></div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+
         </div>
         <!--/ eCommerce statistic -->
     </div>
