4a9d3ab5bd282525364de80d3c9824f2568e5000
unknown <mahi_rbs>
2021-09-13 01:34:29 +0600

    ..

--- web/app/Http/Controllers/CornController.php
+++ web/app/Http/Controllers/CornController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
 use Illuminate\Support\Facades\DB;
 
 class CornController extends Controller
@@ -18,7 +18,7 @@ class CornController extends Controller
 
         $auth_id = Auth::id();
         $lists = DB::table('PRD_LISTINGS')
-        ->select('PRD_LISTINGS.PK_NO','PRD_LISTINGS.PROPERTY_FOR','PRD_LISTINGS.F_PROPERTY_TYPE_NO','PRD_LISTINGS.F_PROPERTY_CONDITION','PRD_LISTING_VARIANTS.PROPERTY_SIZE','PRD_LISTING_VARIANTS.BEDROOM','PRD_LISTING_VARIANTS.BATHROOM','PRD_LISTING_VARIANTS.TOTAL_PRICE','PRD_LISTINGS.F_AREA_NO','PRD_LISTINGS.MAX_SHARING_PERMISSION','PRD_LISTINGS.IS_VERIFIED','PRD_LISTINGS.F_CITY_NO')
+        ->select('PRD_LISTINGS.PK_NO','PRD_LISTINGS.PROPERTY_FOR','PRD_LISTINGS.F_PROPERTY_TYPE_NO','PRD_LISTINGS.F_PROPERTY_CONDITION','PRD_LISTING_VARIANTS.PROPERTY_SIZE','PRD_LISTING_VARIANTS.BEDROOM','PRD_LISTING_VARIANTS.BATHROOM','PRD_LISTING_VARIANTS.TOTAL_PRICE','PRD_LISTINGS.F_AREA_NO','PRD_LISTINGS.MAX_SHARING_PERMISSION','PRD_LISTINGS.IS_VERIFIED','PRD_LISTINGS.F_CITY_NO', 'PRD_LISTINGS.F_USER_NO')
         ->join('WEB_USER', 'WEB_USER.PK_NO','PRD_LISTINGS.F_USER_NO')
         ->join('PRD_LISTING_VARIANTS', 'PRD_LISTING_VARIANTS.F_LISTING_NO','PRD_LISTINGS.PK_NO')
         ->where('PRD_LISTINGS.STATUS',10)
@@ -30,6 +30,7 @@ class CornController extends Controller
 
         if($lists && count($lists) > 0 ){
             foreach ($lists as $key => $list) {
+                $i = 0;
                 $limit = $list->MAX_SHARING_PERMISSION + 20;
                 $seekers = DB::table('PRD_REQUIREMENTS')
                 ->select('PRD_REQUIREMENTS.PK_NO','PRD_REQUIREMENTS.F_USER_NO','PRD_REQUIREMENTS.F_AREAS','PRD_REQUIREMENTS.F_PROPERTY_TYPE_NO','PRD_REQUIREMENTS.PROPERTY_FOR', 'PRD_REQUIREMENTS.MIN_SIZE','PRD_REQUIREMENTS.MAX_SIZE', 'PRD_REQUIREMENTS.MIN_BUDGET','PRD_REQUIREMENTS.MAX_BUDGET', 'PRD_REQUIREMENTS.BEDROOM','PRD_REQUIREMENTS.PROPERTY_CONDITION', 'PRD_REQUIREMENTS.F_CITY_NO', 'PRD_REQUIREMENTS.F_PROPERTY_CONDITION')
@@ -38,17 +39,22 @@ class CornController extends Controller
                 // ->where('PRD_REQUIREMENTS.F_PROPERTY_TYPE_NO', $list->F_PROPERTY_TYPE_NO)
                 // ->where('PRD_REQUIREMENTS.PROPERTY_FOR', $list->PROPERTY_FOR)
                 ->where('PRD_REQUIREMENTS.F_CITY_NO', $list->F_CITY_NO)
+                ->orderBy('PRD_REQUIREMENTS.MODIFIED_AT', 'DESC')
                 ->limit($limit)
                 ->get();
                 if($seekers && count($seekers) > 0 ){
                     foreach ($seekers as $key1 => $list1) {
-                        $total_val = 40;
-                        $req_max_size = $list1->MAX_SIZE + 50;
-                        $req_min_size = $list1->MIN_BUDGET - 50;
-                        $ares   = json_decode($list1->F_AREAS);
-                        $conds  = json_decode($list1->F_PROPERTY_CONDITION);
+                        $total_val = 80;
+                        $req_max_size   = $list1->MAX_SIZE + 50;
+                        $req_min_size   = $list1->MIN_SIZE - 50;
+                        $req_max_budget = $list1->MAX_BUDGET - 100;
+                        $req_min_budget = $list1->MIN_BUDGET - 100;
+                        $bedroom = json_decode($list1->BEDROOM);
+                        $ares    = json_decode($list1->F_AREAS);
+                        $conds   = json_decode($list1->F_PROPERTY_CONDITION);
+
                         if(in_array($list->F_AREA_NO, $ares) && in_array($list->F_AREA_NO, $conds)){
-                            $insertData = array([
+                            $insertData = [
                                 'F_LISTING_NO'  => $list->PK_NO,
                                 'F_COMPANY_NO'  => $list->F_USER_NO,
                                 'F_USER_NO'     => $list1->F_USER_NO,
@@ -58,24 +64,40 @@ class CornController extends Controller
                                 'PROPERTY_TYPE' => 20,
                                 'AREA'          => 20,
                                 'PROPERTY_CONDITION' => 20
-                             ]);
+                             ];
 
-                             if($list->PROPERTY_SIZE < $req_max_size &&  $list->PROPERTY_SIZE > $req_min_size ){
+                            if($list->PROPERTY_SIZE < $req_max_size &&  $list->PROPERTY_SIZE > $req_min_size ){
                                 $total_val += 10;
                                 $insertData['SIZE'] = 10;
-                             }
+                            }
 
-                            //  ['F_LISTING_NO' => $list->PK_NO, 'F_COMPANY_NO' => $list->F_USER_NO, 'F_USER_NO' => $list1->F_USER_NO, 'CREATED_AT' => date('Y-m-d H::s'), 'CREATED_BY' => $auth_id , 'PROPERTY_FOR' => 20 , 'PROPERTY_TYPE' => 20, 'AREA' => 20, 'SIZE' => , 'BEDROOM' => ,'BATHROOM' => ,'TOTAL_PRICE' => ,'PROPERTY_CONDITION' => 20 ,'TOTAL_VAL' => ]
+                            if($list->TOTAL_PRICE < $req_max_budget &&  $list->TOTAL_PRICE > $req_min_budget ){
+                                $total_val += 10;
+                                $insertData['TOTAL_PRICE'] = 10;
+                            }
 
-                            // DB::table('PRD_SUGGESTED_PROPERTY_TEMP')->insert($insertData);
+                            if($bedroom && $bedroom['0'] == 'any'){
+                                $total_val += 10;
+                                $insertData['BEDROOM'] = 10;
+                            }elseif(in_array($list->BEDROOM, $bedroom)){
+                                $total_val += 10;
+                                $insertData['BEDROOM'] = 10;
+                            }
 
-dd($list1);
+                            $insertData['TOTAL_VAL'] = $total_val;
+                            DB::table('PRD_SUGGESTED_PROPERTY_TEMP')->insert($insertData);
+                            $i++;
+
                         }
+
+                        if($list->MAX_SHARING_PERMISSION < $i ){break;}
                     }
                 }
 
             }
         }
+
+        dd($lists);
 
     }
 
